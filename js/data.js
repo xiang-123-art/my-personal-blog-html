4eeaadfb2fe21f299f631a84f91f3b43a04d721b
Xiang <1599474399@qq.com>
2025-10-06 00:35:55 +0800

    feat: initial commit - add all blog pages (home, articles, about, styles)

---
+++ js/data.js
@@ -0,0 +1,24 @@
+// js/data.js
+const articles = [
+  {
+    id: 1,
+    title: "My first blog post",
+    summary: "This is the first article of my blog system. Welcome to my world!",
+    content: "In this blog, I plan to share my thoughts on programming, the challenges I encountered while learning new tools, and the progress I have made along the way. I hope this will be a useful space for reflection and learning, and perhaps it can also help those on a similar path.",
+    date: "2025-06-01"
+  },
+  {
+    id: 2,
+    title: "My Experience in Learning Front-End Development",
+    summary: "Share some of my experiences in learning HTML, CSS and JavaScript.",
+    content: "During my learning process of front-end development, I have realized that practice is the most crucial aspect. No matter how many front-end tutorials you have read or how many programming languages you have mastered, if you don't actually start writing projects and doing exercises, it will be very difficult to truly master this technology. Front-end development is a field that places great emphasis on 'producing results'. Only through continuous practice can you find your own development rhythm and also gain more confidence in facing complex practical requirements.",
+    date: "2025-06-06"
+  },
+  {
+    id: 3,
+    title: "The Importance of Configuration Management",
+    summary: "Why is version control and configuration management so crucial in software development?",
+    content: "Configuration management facilitates team collaboration, tracks changes, and ensures software quality.",
+    date: "2025-06-10"
+  }
+];
