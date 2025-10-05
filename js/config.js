c63e4ac4f0658907df334451ff9b6ed78287c332
Huxiang <1599474399@qq.com>
2025-10-06 04:01:11 +0800

    重新提交

---
+++ js/config.js
@@ -0,0 +1,9 @@
+const SITE_CONFIG = {
+  title: "My Personal Blog",
+  navLinks: [
+    { name: "🏠 Homepage", url: "index.html" },
+    { name: "📝 Article List", url: "articles.html" },
+    { name: "👤 About Me", url: "about.html" }
+  ],
+  footerText: "© 2025 My Personal Blog. All rights reserved."
+};
